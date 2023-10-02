6363a9a279a53c203c7007395adfb4cf87c8be7c
seonydg <baebio@naver.com>
2023-10-02 18:27:12 +0900

    feat : test

--- vgg Cat & Dog image classification/utils.py
+++ vgg Cat & Dog image classification/utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
 def calculate_accuracy(y_pred, y):
     top_pred = y_pred.argmax(1, keepdim=True)
     correct = top_pred.eq(y.view_as(top_pred)).sum()
@@ -11,3 +15,34 @@ def epoch_time(start_time, end_time):
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
 
     return elapsed_mins, elapsed_secs
+
+def normalize_image(image):
+    image_min = image.min
+    image_max = image.max
+    image.clamp_(min=image_min, max=image_max)
+    image.add_(-image_min.div_(image_max - image_min + 1e-5))
+
+    return image
+
+def show_correct_images(correct, classes, n_images, normalize=True):
+    rows = int(np.sqrt(n_images))
+    cols = int(np.sqrt(n_images))
+
+    fig = plt.figure(figsize=(12, 8))
+    for i in range(rows * cols):
+        ax = fig.add_subplot(rows, cols, i+1)
+        image, true_label, probs = correct[i]
+        image = image.permute(1, 2, 0)
+        true_prob = probs[true_label]
+        correct_prob, correct_label = torch.max(probs, dim=0)
+        true_class = classes[true_label]
+        correct_class = classes[correct_label]
+
+        if normalize:
+            image = normalize_image(image)
+        
+        ax.imshow(image.cpu().numpy())
+        ax.set_title(f'true label: {true_class} ({true_prob:.3f}) | pred label: {correct_class} ({correct_prob:.3f})')
+        ax.axis('off')
+    
+    fig.subplots_adjust(hspace=0.4)
